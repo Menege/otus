bba2015a718a798d65634f35f0c67fda7545c260
Ruslan Efremov <efrerus@gmail.com>
2024-09-26 11:48:48 +0300

    javascript-9 add fix

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   PORT: process.env.PORT || 3000,
-  JWT_SECRET: process.env.JWT_SECRET || "supersecret",
+  JWT_SECRET: process.env.JWT_SECRET || "supersecretkey",
   DB_CONNECTION:
     process.env.DB_CONNECTION || "postgres://postgres:1@localhost/leetcode",
 };
